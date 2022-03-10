b08eea43d2d21e6200dcc2cf2ff1eb3ba21613d5
JR <www.aereregion.webs.com@live.co.uk>
2022-03-10 15:42:38 +0000

    Make CoroutineSourceCachePopulator.kt open

--- populator/coroutine-source-cache/src/main/java/com/cube/fusion/populator/coroutinesourcecache/CoroutineSourceCachePopulator.kt
+++ populator/coroutine-source-cache/src/main/java/com/cube/fusion/populator/coroutinesourcecache/CoroutineSourceCachePopulator.kt
@@ -23,7 +23,7 @@ import kotlinx.coroutines.launch
  * @param sources the collection of sources to attempt to fetch / cache data in.
  *  Be aware that they will be checked in collection order.
  */
-class CoroutineSourceCachePopulator(val scopeFactory: () -> CoroutineScope, val sources: Collection<FusionDataSource<Page>>) : FusionDisplayPopulator {
+open class CoroutineSourceCachePopulator(val scopeFactory: () -> CoroutineScope, val sources: Collection<FusionDataSource<Page>>) : FusionDisplayPopulator {
 	constructor(scopeFactory: () -> CoroutineScope, vararg sources: FusionDataSource<Page>): this(scopeFactory, sources.toList())
 	override fun populateDisplayFromUri(screenLink: String, target: FusionDisplayTarget, loadingIndicator: FusionLoadingIndicator) {
 		loadingIndicator.setLoadingState(true)

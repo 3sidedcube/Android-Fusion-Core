86ca93f90695ba8674e79c628d27cad6ba544258
JR-Mitchell <8377714+JR-Mitchell@users.noreply.github.com>
2023-04-12 08:33:10 +0100

    feat(tests): adds test utilities to JsonTestUtils.kt for testing JSON serialisation

--- core/src/test/java/com/cube/fusion/core/JsonTestUtils.kt
+++ core/src/test/java/com/cube/fusion/core/JsonTestUtils.kt
@@ -2,7 +2,12 @@ package com.cube.fusion.core
 
 import com.cube.fusion.core.resolver.ViewResolver
 import com.cube.fusion.core.utils.objectMapper
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.core.util.DefaultIndenter
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import org.assertj.core.api.Assertions
 
 /**
@@ -12,6 +17,26 @@ import org.assertj.core.api.Assertions
  * Copyright ® 3SidedCube. All rights reserved.
  */
 object JsonTestUtils {
+
+	private class CustomPrettyPrinter: DefaultPrettyPrinter() {
+		init {
+			_arrayIndenter = DefaultIndenter("\t", "\n")
+			_objectIndenter = DefaultIndenter("\t", "\n")
+		}
+
+		override fun createInstance(): DefaultPrettyPrinter {
+			return CustomPrettyPrinter()
+		}
+
+		override fun writeObjectFieldValueSeparator(g: JsonGenerator?) {
+			g?.writeRaw(": ")
+		}
+	}
+
+	private val customPrettyPrinter by lazy {
+		CustomPrettyPrinter()
+	}
+
 	/**
 	 * Assert that an object is equal to the JSON deserialisation of its JSON data
 	 *
@@ -25,5 +50,23 @@ object JsonTestUtils {
 		val mapper = resolvers?.objectMapper() ?: ObjectMapper()
 		val readObj = mapper.readValue(inStr, T::class.java)
 		Assertions.assertThat(readObj).usingRecursiveComparison().isEqualTo(inObj)
+	}
+
+	/**
+	 * Assert that an object correctly serialises to JSON
+	 *
+	 * @param T The type of the object to serialise.
+	 * @param inObj The instance of [T] that should serialise to [inStr].
+	 * @param inStr The JSON representation of [inObj].
+	 * @param resolvers The list of view resolvers to use for serialisation, if necessary.
+	 */
+	fun <T> assertEqualityFromJsonSerialisation(inObj: T, inStr: String, resolvers: List<ViewResolver>? = null)
+	{
+		val mapper = resolvers?.objectMapper() ?: ObjectMapper()
+		mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
+		mapper.setDefaultPrettyPrinter(customPrettyPrinter)
+		mapper.enable(SerializationFeature.INDENT_OUTPUT)
+		val writtenString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(inObj)
+		Assertions.assertThat(writtenString).isEqualTo(inStr)
 	}
 }

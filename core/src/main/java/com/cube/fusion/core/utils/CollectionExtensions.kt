00aa2b54c4e8922ede91364943ccb47270a4adfb
JR-Mitchell <8377714+JR-Mitchell@users.noreply.github.com>
2023-05-15 11:58:00 +0100

    chore(preprocessing): removes the now-unnecessary Collection<FusionDataPreprocessor>.preprocess extension function

--- core/src/main/java/com/cube/fusion/core/utils/CollectionExtensions.kt
+++ core/src/main/java/com/cube/fusion/core/utils/CollectionExtensions.kt
@@ -1,6 +1,5 @@
 package com.cube.fusion.core.utils
 
-import com.cube.fusion.core.processor.FusionDataPreprocessor
 import com.cube.fusion.core.resolver.ViewResolver
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
@@ -22,21 +21,5 @@ object CollectionExtensions {
 			registerSubtypes(it.resolveView())
 		}
 		disable(SerializationFeature.FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS)
-	}
-
-	/**
-	 * Convenience method to preprocess a model with a series of preprocessors
-	 * The processing steps will be applied in order of iteration
-	 *
-	 * @param model The model to preprocessing
-	 * @param T The type of [model]
-	 * @return An instance of [T] created by chained application of the preprocessors in [this] in iteration order to [model]
-	 */
-	fun <T> Collection<FusionDataPreprocessor<T>>.preprocess(model: T): T {
-		var outModel: T = model
-		forEach { processor ->
-			outModel = processor.preprocess(outModel)
-		}
-		return outModel
 	}
 }
